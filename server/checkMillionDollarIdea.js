91d6b31055bf3ddb5d8bf1f47bffe97abdb47edd
Laksito Pamilih <laksito@protonmail.com>
2023-09-15 15:32:32 +0700

    add lint script in package.json

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,4 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = () => {}
 
 // Leave this exports assignment so that the function can be used elsewhere
-module.exports = checkMillionDollarIdea;
+module.exports = checkMillionDollarIdea
